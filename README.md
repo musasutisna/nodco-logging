4a061c6730ab251d684687e0f705c06c26b9551d
Musa Sutisna <mus4.sutisn4@gmail.com>
2026-05-15 08:21:29 +0700

    Support feature rotation by date or size file log

--- README.md
+++ README.md
@@ -7,26 +7,39 @@ Nodejs config api is package to make easier configuration nodejs configuration i
 Lets install nodco-logging with npm
 
 ```bash
-npm install --save @musasutisna/nodco-api
+npm install --save @musasutisna/nodco-logging
 ```
 
 ## How to initialize
 
 ```js
-nodcoLogginConfig(
+const nodcoLoggingConfig = require('@musasutisna/nodco-logging');
+
+nodcoLoggingConfig(
   {
     // default config here
   }
 )
 ```
 
+| Config | Type | Default | Description |
+|:--|:--|:--|:--|
+| dirs | String | logs | Relative path where logs stored. |
+| size | Number | 1024 * 1024 | Max size of file log in bytes, the default is 1 MB. |
+| format | String | date | The format of filename log date 'YYYY-MM-DD' and datetime 'YYYY-MM-DD-HH' |
+| json | Boolean | false | Format line log into json. |
+
+<br/>
+
 | Method | Type | Description |
 |:--|:--|:--|
-| send | async | Manage logs and send into file. |
+| write | async | Manage logs and send into file. |
+
+<br/>
 
 ```js
 send(
-  logIds, // string, unique id log
-  message // string, message will be write into log file
+  ids, // String, The logs unique identity.
+  message // String, The message will be write.
 )
 ```
--- index.js
+++ index.js
@@ -1,52 +1,234 @@
 const fs = require('fs');
+const path = require('path');
 
 /**
  * Create a log configuration.
  *
- * @param   object    { ids: { date, path, stream } }
- * @return  object
+ * @param {Object} [options={}] - Configuration options for the logging.
+ * @param {String} [options.path='logs'] - Relative path where logs stored.
+ * @param {Number} [options.size=1024 * 1024] - Max size of file log in bytes, the default is 1 MB.
+ * @param {String} [options.format='date'] - The format of filename log date 'YYYY-MM-DD' and datetime 'YYYY-MM-DD-HH'
+ * @param {Boolean} [options.json=false] - Format line log into json.
+ * @returns {Object}
  */
-module.exports = function nodcoLoggingConfig(config = {}) {
+module.exports = function nodcoLoggingConfig({
+    dirs = 'logs',
+    size = 1024 * 1024,
+    format = 'date',
+    json = false
+} = {}) {
   /**
-   * Manage log and write log into file.
+   * The list of current logs config.
    *
-   * @param   string
-   * @param   string
-   * @return  void
+   * @typedef {Object} logs
+   * @property {Object} [ids.date] - The date component when log created.
+   * @property {String} [ids.path] - Base path location to file log without index.
+   * @property {Number} [ids.index] - The file index of log.
+   * @property {Number} [ids.initial_size] - Size of initial file log in bytes.
+   * @property {Number} [ids.current_size] - The calculate of current file log in bytes.
+   * @property {Object} [ids.stream] - File log stream.
    */
-  async function send(ids, msg) {
-    let selectedLog = config[ids];
+  const logs = {};
 
-    if (typeof selectedLog === 'undefined') {
-      selectedLog = {
-        date: null,
-        path: null, 
-        stream: null
-      };
+  /**
+   * Write logs.
+   *
+   * @param {String} ids - The logs unique identity.
+   * @param {String|Object} message - The message will be write.
+   * @returns {Promise<void>}
+   */
+  async function write(ids, message) {
+    const log = getLogger(ids);
+    const timestamp = (new Date).toISOString();
+
+    let line;
+
+    if (json) {
+      message._t = timestamp;
+
+      line = JSON.stringify(message);
+    } else {
+      line = `${timestamp} : ${message}`;
     }
 
-    const d = new Date();
+    return new Promise((resolve, reject) => {
+      log.stream.write(line + '\n', err => {
+        if (err) {
+          reject(err);
+        } else {
+          logs[ids].current_size += logs[ids].stream.bytesWritten
 
-    if (selectedLog.date === null || (d.getDate() !== selectedLog.date.getDate())) {
-      if (selectedLog.stream) {
-        selectedLog.stream.end();
+          resolve();
+        }
+      });
+    });
+  }
+
+  /**
+   * Get the logs by ids.
+   *
+   * @param {String} ids - The logs unique identity.
+   * @returns {Object}
+   */
+  function getLogger(ids) {
+    let newLoggerDate = null;
+    let newLoggerIndex = 0;
+
+    if (typeof logs[ids] === 'undefined') {
+      // add new log
+      logs[ids] = {};
+
+      newLoggerDate = getDateComponents(new Date());
+    } else {
+      const isShouldRotateByDate = shouldRoateByDate(logs[ids].date);
+
+      if (isShouldRotateByDate) {
+        newLoggerDate = isShouldRotateByDate;
+      } else {
+        const isShouldRotateBySize = logs[ids].current_size >= size;
+
+        if (isShouldRotateBySize) {
+          newLoggerDate = logs[ids].date;
+          newLoggerIndex = logs[ids].index + 1;
+        }
+      }
+    }
+
+    if (newLoggerDate !== null) {
+      findAvailableFile(ids, newLoggerDate, newLoggerIndex);
+    }
+
+    return logs[ids];
+  }
+
+  /**
+   * Generate date components.
+   *
+   * @param {Object} date - The date will be generate.
+   * @returns {Object}
+   */
+  function getDateComponents(date) {
+    return {
+      year: date.getFullYear(),
+      month: padDate(date.getMonth() + 1),
+      day: padDate(date.getDate()),
+      hour: padDate(date.getHours())
+    };
+  }
+
+  /**
+   * Pad the date component with leading zero.
+   *
+   * @param {Number} n - The component will be leading.
+   * @returns {String}
+   */
+  function padDate(n) {
+    return String(n).padStart(2, '0');
+  }
+
+  /**
+   * Find available file to use for log.
+   *
+   * @param {String} ids - The logs unique identity.
+   * @param {Object} datelog - Date of log.
+   * @param {Number} index - Index of log if rotation. 
+   * @returns {String}
+   */
+  function findAvailableFile(ids, datelog, index = 0) {
+    let base;
+
+    if (format === 'datetime') {
+      base = `${datelog.year}-${datelog.month}-${datelog.day}-${datelog.hour}`;
+    } else {
+      base = `${datelog.year}-${datelog.month}-${datelog.day}`;
+    }
+
+    logs[ids].path = base;
+
+    let filesize = 0;
+    let filepath = '';
+
+    while (true) {
+      filepath = path.join(dirs, ids, `${base}.${index}.log`);
+
+      if (fs.existsSync(filepath)) {
+        filesize = fs.statSync(filepath).size;
+
+        if (filesize >= size) {
+          index += 1;
+
+          continue;
+        }
       }
 
-      selectedLog.date = d;
-      selectedLog.path = `logs/${ids}/${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}.log`;
-      selectedLog.stream = fs.createWriteStream(selectedLog.path, {
-        flags: 'a+'
-      });
+      logs[ids].index = index;
+
+      break;
     }
 
-    const timestamp = d.toLocaleString('en-CA', {
-      hourCycle: 'h23'
-    });
+    // there was existing stream
+    // close that stream
+    if (logs[ids].stream !== undefined && logs[ids].stream !== null) {
+      logs[ids].stream.end();
+    }
 
-    await selectedLog.stream.write(`${timestamp} : ${msg}` + "\n");
+    logs[ids].date = datelog;
+    logs[ids].path = base;
+    logs[ids].initial_size = filesize;
+    logs[ids].current_size = filesize;
+    logs[ids].index = index;
+    logs[ids].stream = createStream(filepath);
+  }
+
+  /**
+   * Create log stream file.
+   *
+   * @param {String} filepath - The path of log file.
+   * @returns {Object}
+   */
+  function createStream(filepath) {
+    ensureDir(filepath);
+
+    return fs.createWriteStream(filepath, {
+      flags: 'a'
+    });
+  }
+
+  /**
+   * Ensure directory of log is exists then created.
+   *
+   * @param {String} filePath - The path of log file.
+   * @returns {void}
+   */
+  function ensureDir(filePath) {
+    fs.mkdirSync(path.dirname(filePath), { recursive: true });
+  }
+
+  /**
+   * To determine is time to rotation log file.
+   *
+   * @param {String} oldDate - The log date will be compared.
+   * @returns {Object}
+   */
+  function shouldRoateByDate(oldDate) {
+    const newDate = getDateComponents(new Date());
+
+    if (
+      oldDate.year !== newDate.year ||
+      oldDate.month !== newDate.month ||
+      oldDate.day !== newDate.day
+    ) {
+      return true;
+    }
+
+    if (format === 'datetime' && oldDate.hour !== newDate.hour) {
+      return newDate;
+    }
+
+    return false;
   }
 
   return {
-    send
+    write
   };
 };
